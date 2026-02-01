8717d11857f07b1e6389f14c612da013755bef79
tote10 <motialemu9@gmail.com>
2026-02-01 18:47:30 +0300

    feat: Implement a basic Tornado web server with static, basic, query string, and resource handlers, and add an `index.html` page.

---
+++ stage-01-http-fundamentals/raw-server.py
@@ -0,0 +1,34 @@
+import tornado
+import tornado.ioloop
+
+class basicRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("hello world!!")
+class staticRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+class queryStringRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        n=int(self.get_argument("n"))
+        r = "odd" if n%2 else "even"
+        self.write("the number "+ str(n) + " is " + r)
+
+class resourceRequestHandler(tornado.web.RequestHandler):
+    def get(self,n):
+        self.write("querying tweet with id " + n)
+
+if __name__ == "__main__":
+    app=tornado.web.Application([
+        (r"/",basicRequestHandler),
+        (r"/static",staticRequestHandler),
+        (r"/isEven",queryStringRequestHandler),
+        (r"/tweet/([0-9]+)",resourceRequestHandler)
+
+    ])
+    app.listen(8881)
+    print("hey i'm listiening on port 8000")
+    tornado.ioloop.IOLoop.current().start()
+
+
+
