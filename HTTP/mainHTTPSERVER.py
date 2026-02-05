34827979ccfdd6a13673dd7589744daa56759b7b
tote10 <motialemu9@gmail.com>
2026-02-05 13:23:13 +0300

    feat: Implement basic HTTP servers for handling simple requests, routing, and query parameters.

--- HTTP/mainHTTPSERVER.py
+++ HTTP/mainHTTPSERVER.py
@@ -1,24 +1,33 @@
-from http.server import HTTPServer,BaseHTTPRequestHandler
-import time
+from http.server import BaseHTTPRequestHandler ,HTTPServer
 
-
-HOST = "localhost"
-PORT = 9999
-class requestHandler(BaseHTTPRequestHandler):
+class simpleRequesrHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type","text/plain")
         self.end_headers()
-        self.wfile.write(b"<html><body><h1>Main Page</h1></body></html>")
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        date = time.strftime("%Y-%m-%d %H:%M:%S" , time.localtime(time.time()))
-        self.wfile.write(bytes(f'{{"time" : "{date}"}}', "utf-8"))
-server = HTTPServer((HOST,PORT),requestHandler)
-print("server started")
-server.serve_forever()
-
-server.server_close()
-print("server closed")
+        self.wfile.write(b"Hello World")
+        print("GET request received")
+        print("command is" + self.command)
+        print("path is" + self.path)
+        print("request version is ",self.request_version)
+        print("headers is ",self.headers)
+        print("server is " , self.server)
+        print("connection is " , self.connection)
+       
+        print("self is " , self.wfile)
+        print("close connection is " , self.close_connection)
+        print("command is " , self.command)
+        print("path is " , self.path)
+        print("request version is " , self.request_version)
+        print("headers is " , self.headers)
+        print("client address is " , self.client_address)
+        
+        print("server is " , self.server)
+        print("connection is " , self.connection)
+        print("rfile is " , self.rfile)
+        print("wfile is " , self.wfile)
+        print("close connection is " , self.close_connection)
+if __name__ == "__main__":
+    server = HTTPServer(("localhost",8000),simpleRequesrHandler)
+    print("Server started on port 8000")
+    server.serve_forever()
